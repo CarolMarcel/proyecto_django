01e33035621951c265d123bbd964ca206b7350e4
Carol Marcel <cg.marcel@gmail.com>
2025-09-08 17:15:41 -0300

    Comentarios en home, admin, style y gracias

--- portafolio/admin.py
+++ portafolio/admin.py
@@ -1,8 +1,14 @@
+# Importa el panel de administración de Django 
 from django.contrib import admin
+# Importa el modelo ContactMessage desde models.py
 from .models import ContactMessage
 
+# Registra el modelo en el panel de administración de Django
+# usando un decorador (@admin.register).
 @admin.register(ContactMessage)
 class ContactMessageAdmin(admin.ModelAdmin):
+    # Campos que se verán como columnas en la lista de mensajes
     list_display = ("nombre", "email", "creado")
+    # Campos que serán de solo lectura en el detalle de un registro
     readonly_fields = ("creado",)
 
